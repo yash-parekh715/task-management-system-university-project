d8921b43f146797fcea61b6c65c544afd8aaf605
Bobby L <rioted@gmail.com>
2018-10-18 04:13:24 -0500

    Update db_connect.php
    Included a comment regarding the default values of the database.

--- db_connect.php
+++ db_connect.php
@@ -4,3 +4,4 @@ function db(){
     $link = mysqli_connect("localhost", "root", "", "todo") or die("couldn't connect to database");
     return $link;
 }
+/* this database is designed to run with default settings: it is stored locally, the user is root and pass is a null string, and the database title is todo */
