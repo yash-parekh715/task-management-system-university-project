821569fbc8f47e19ab9193eb5de6c33efb5d4e08
Bobby L <rioted@gmail.com>
2018-10-18 04:27:35 -0500

    Added tasks.sql file
    This is the SQL script that can be imported into phpMyAdmin in order to create the correct table in order to make this work.

---
+++ tasks.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `todoTitle` varchar(200) COLLATE utf8_bin NOT NULL,
+  `todoDescription` text COLLATE utf8_bin NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+ALTER TABLE `tasks`
+  ADD UNIQUE KEY `id` (`id`);
