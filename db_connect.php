6808e66c010201ebda6f5be359dfb8b66bf56215
Bobby L <rioted@gmail.com>
2018-10-18 19:44:12 -0500

    Delete db_connect.php
    Not required for this branch.

--- db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-function db(){
-    global $link;
-    $link = mysqli_connect("localhost", "root", "", "todo") or die("couldn't connect to database");
-    return $link;
-}
-/* this database is designed to run with default settings: it is stored locally, the user is root and pass is a null string, and the database title is todo */
